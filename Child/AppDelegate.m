0cc6ceadf4bb8fa2fb4f158091864f68a69d4fd1
AdilVirani <me@adilvirani.io>
2015-10-24 22:24:49 -0700

    yo

--- Child/AppDelegate.m
+++ Child/AppDelegate.m
@@ -8,6 +8,17 @@
 
 #import "AppDelegate.h"
 
+//model
+#import "YALTabBarItem.h"
+
+//controller
+#import "YALFoldingTabBarController.h"
+
+//helpers
+#import "YALAnimatingTabBarConstants.h"
+
+#import "AppDelegate.h"
+
 @interface AppDelegate ()
 
 @end
@@ -17,7 +28,50 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    [self setupYALTabBarController];
+    
     return YES;
+}
+
+- (void)setupYALTabBarController {
+    YALFoldingTabBarController *tabBarController = (YALFoldingTabBarController *) self.window.rootViewController;
+    
+    //prepare leftBarItems
+    YALTabBarItem *item1 = [[YALTabBarItem alloc] initWithItemImage:[UIImage imageNamed:@"chats_icon"]
+                                                      leftItemImage:nil
+                                                     rightItemImage:nil];
+    
+    
+    YALTabBarItem *item2 = [[YALTabBarItem alloc] initWithItemImage:[UIImage imageNamed:@"profile_icon"]
+                                                      leftItemImage:[UIImage imageNamed:@"edit_icon"]
+                                                     rightItemImage:nil];
+    
+    tabBarController.leftBarItems = @[item1, item2];
+    
+    //prepare rightBarItems
+    YALTabBarItem *item3 = [[YALTabBarItem alloc] initWithItemImage:[UIImage imageNamed:@"nearby_icon"]
+                                                      leftItemImage:[UIImage imageNamed:@"search_icon"]
+                                                     rightItemImage:[UIImage imageNamed:@"new_chat_icon"]];
+    
+    
+    YALTabBarItem *item4 = [[YALTabBarItem alloc] initWithItemImage:[UIImage imageNamed:@"settings_icon"]
+                                                      leftItemImage:nil
+                                                     rightItemImage:nil];
+    
+    tabBarController.rightBarItems = @[item3, item4];
+    
+    tabBarController.centerButtonImage = [UIImage imageNamed:@"plus_icon"];
+    
+    tabBarController.selectedIndex = 2;
+    
+    //customize tabBarView
+    tabBarController.tabBarView.extraTabBarItemHeight = YALExtraTabBarItemsDefaultHeight;
+    tabBarController.tabBarView.offsetForExtraTabBarItems = YALForExtraTabBarItemsDefaultOffset;
+    tabBarController.tabBarView.backgroundColor = [UIColor colorWithRed:94.0/255.0 green:91.0/255.0 blue:149.0/255.0 alpha:1];
+    tabBarController.tabBarView.tabBarColor = [UIColor colorWithRed:72.0/255.0 green:211.0/255.0 blue:178.0/255.0 alpha:1];
+    tabBarController.tabBarViewHeight = YALTabBarViewDefaultHeight;
+    tabBarController.tabBarView.tabBarViewEdgeInsets = YALTabBarViewHDefaultEdgeInsets;
+    tabBarController.tabBarView.tabBarItemsEdgeInsets = YALTabBarViewItemsDefaultEdgeInsets;
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment this line to define a global platform for your project
+platform :ios, "7.0"
+# Uncomment this line if you're using Swift
+# use_frameworks!
+
+target 'Child' do
+
+pod 'RESideMenu', '~> 4.0.7'
+pod 'PNChart'
+pod 'FoldingTabBar', '~> 1.0.1'
+
+end
+
+target 'ChildTests' do
+
+end
+
+target 'ChildUITests' do
+
+end
+
